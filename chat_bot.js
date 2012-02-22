ad589099432d0e4b5a9340be33de8f2b7d38f7e6
Austin Brace <atbrace@gmail.com>
2012-02-22 09:40:03 +0400

    dont ask about dubstep

--- chat_bot.js
+++ chat_bot.js
@@ -119,8 +119,14 @@ bot.on('speak', function (data) {
 		}
 	}
 	else if(data.name != 'syzbot' && data.name != '#EVE') {
-		if (text.match(/bro/i)) {
+		if (text.match(/\bbro\b/)) {
 			bot.speak("BRO");
+		}
+		else if (text.match(/good boy/i)) {
+			bot.speak(":D");
+		}
+		else if (text.match(/dubstep/i) && (text.match(/play/i) || text.match(/cool/i) || text.match(/ok/i) || text.match(/like/i))) {
+			bot.speak("Sorry bro, no dubstep in here. Please read the room rules.");
 		}
 		else if (text.match(/syzbot/i)) {
 			if (text.match(/engage partymode/i) && (mods.indexOf(data.userid) > -1)) {
@@ -337,47 +343,12 @@ bot.on('speak', function (data) {
 	}
 	
 	function strip_tags (input, allowed) {
-    // Strips HTML and PHP tags from a string  
-    // 
-    // version: 1109.2015
-    // discuss at: http://phpjs.org/functions/strip_tags
-    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   improved by: Luke Godfrey
-    // +      input by: Pul
-    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: Onno Marsman
-    // +      input by: Alex
-    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +      input by: Marc Palau
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +      input by: Brett Zamir (http://brett-zamir.me)
-    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: Eric Nagel
-    // +      input by: Bobby Drake
-    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: Tomasz Wesolowski
-    // +      input by: Evertjan Garretsen
-    // +    revised by: Rafal Kukawski (http://blog.kukawski.pl/)
-    // *     example 1: strip_tags('<p>Kevin</p> <b>van</b> <i>Zonneveld</i>', '<i><b>');
-    // *     returns 1: 'Kevin <b>van</b> <i>Zonneveld</i>'
-    // *     example 2: strip_tags('<p>Kevin <img src="someimage.png" onmouseover="someFunction()">van <i>Zonneveld</i></p>', '<p>');
-    // *     returns 2: '<p>Kevin van Zonneveld</p>'
-    // *     example 3: strip_tags("<a href='http://kevin.vanzonneveld.net'>Kevin van Zonneveld</a>", "<a>");
-    // *     returns 3: '<a href='http://kevin.vanzonneveld.net'>Kevin van Zonneveld</a>'
-    // *     example 4: strip_tags('1 < 5 5 > 1');
-    // *     returns 4: '1 < 5 5 > 1'
-    // *     example 5: strip_tags('1 <br/> 1');
-    // *     returns 5: '1  1'
-    // *     example 6: strip_tags('1 <br/> 1', '<br>');
-    // *     returns 6: '1  1'
-    // *     example 7: strip_tags('1 <br/> 1', '<br><br/>');
-    // *     returns 7: '1 <br/> 1'
-    allowed = (((allowed || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join(''); // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
-    var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
-        commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
-    return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
-        return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
-    });
+		allowed = (((allowed || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join(''); // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
+		var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
+			commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+		return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+			return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+		});
 }
 	
 });
